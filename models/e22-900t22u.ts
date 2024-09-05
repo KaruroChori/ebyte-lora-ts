d67755b32106d55adbb16f6c51583cd8a2757f71
karurochari <nope>
2024-09-05 23:34:52 +0000

    logic cleanup, and refactoring into a proper client class

--- models/e22-900t22u.ts
+++ models/e22-900t22u.ts
@@ -158,100 +158,128 @@ export const lora_cfg = t.Union([lora_cfg_endpoint, lora_cfg_relay])
 export type lora_cfg_t = Static<typeof lora_cfg>
 
 
-/**
- * Serialize the configuration. 
- * While the original specs allows to update each register on its own, here it is assumed to be in one step.
- * @param cfg a validated configuration json
- * @returns an array to send via serial
- */
-export function serialize_config(cfg: lora_cfg_t): Uint8Array {
-    const tmp = new Uint8Array(3 + 7)
-    tmp[0] = 0xC0
-    tmp[1] = 0x00
-    tmp[2] = 0x08
-    if (cfg.mode === 'relay') {
-        tmp[3] = cfg.src_network
-        tmp[4] = cfg.dst_network
-        tmp[5] = 0x00
+
+export class lora_node {
+    private config: lora_cfg_t
+    private file: unknown
+    mode: 'local' | 'ota' = 'local'
+
+    // biome-ignore lint/suspicious/noExplicitAny: Validation will be performed downstream
+    constructor(file: unknown, opts: any = {}) { this.file = file; this.config = Value.Default(opts.mode === "relay" ? lora_cfg_relay : lora_cfg_endpoint, opts) as lora_cfg_t }
+
+    /**
+     * Serialize the configuration. 
+     * While the original specs allows to update each register on its own, here it is assumed to be in one step.
+     * @param cfg a validated configuration json
+     * @param temporary true if writing on temporary registers
+     * @returns an array to send via serial
+     */
+    static serialize_config(cfg: lora_cfg_t): Uint8Array {
+        const tmp = new Uint8Array(7)
+        if (cfg.mode === 'relay') {
+            tmp[0] = cfg.src_network
+            tmp[1] = cfg.dst_network
+            tmp[2] = 0x00
+        }
+        else {
+            tmp[0] = cfg.address & 0xff00
+            tmp[1] = cfg.address & 0x00ff
+            tmp[2] = cfg.network
+        }
+        tmp[3] = air_rate_s[cfg.radio.air_rate] | mode_s[cfg.serial.mode] << 3 | baud_rate_s[cfg.serial.baud_rate] << 5
+        tmp[4] = tx_power_s[cfg.radio.tx_power] | (cfg.serial.enable_mode_switch ? 1 : 0) << 2 | (cfg.serial.enable_RSSI_cmds ? 1 : 0) << 5 | frame_size_s[cfg.transport.frame_size] << 6
+        tmp[5] = cfg.radio.channel
+        tmp[6] = (cfg.transport.enable_LBT ? 1 : 0) << 4 | (cfg.mode === 'relay' ? 1 : 0) << 5 | (cfg.transport.fixed ? 1 : 0) << 6 | (cfg.transport.enable_RSSI ? 1 : 0) << 7
+        return tmp
     }
-    else {
-        tmp[3] = cfg.address & 0xff00
-        tmp[4] = cfg.address & 0x00ff
-        tmp[5] = cfg.network
+
+    /**
+     * De-serialize a byte array into a configuration json.
+     * While the original specs would allow to read registers in chunks, here it is assumed to be in one operation.
+     * @param array the returned array of bytes
+     * @param skip_sig_check ignore checks on the return header (used for internal tests only)
+     * @returns the de-serialized json
+     */
+    static deserialize_config(array: Uint8Array): lora_cfg_t {
+        if (array.length < 7) throw new Error("Unable to decode an arbitrary received message.")
+        const cfg: lora_cfg_t = { radio: {}, serial: {}, transport: {} } as lora_cfg_t
+
+        cfg.mode = (((array[6] >> 5) & 1) === 0) ? 'endpoint' : 'relay'
+        if (cfg.mode === 'endpoint') {
+            cfg.address = array[0] << 8 + array[1]
+            cfg.network = array[2]
+        }
+        else {
+            cfg.src_network = array[0]
+            cfg.dst_network = array[1]
+        }
+        cfg.radio.air_rate = air_rate_e[array[3] & 1]
+        cfg.serial.mode = mode_e[((array[3] >> 3) & 3)]
+        cfg.serial.baud_rate = baud_rate_e[((array[3] >> 5) & 7)]
+
+        cfg.radio.tx_power = tx_power_e[array[4] & 3]
+        cfg.serial.enable_mode_switch = !(((array[4] >> 2) & 1) === 0)
+        cfg.serial.enable_RSSI_cmds = !(((array[4] >> 5) & 1) === 0)
+        cfg.transport.frame_size = frame_size_e[(array[4] >> 6) & 3]
+
+        cfg.radio.channel = array[5]
+
+        cfg.transport.enable_LBT = !(((array[6] >> 4) & 1) === 0)
+        cfg.transport.fixed = !(((array[6] >> 6) & 1) === 0)
+        cfg.transport.enable_RSSI = !(((array[6] >> 7) & 1) === 0)
+        return cfg
     }
-    tmp[6] = air_rate_s[cfg.radio.air_rate] | mode_s[cfg.serial.mode] << 3 | baud_rate_s[cfg.serial.baud_rate] << 5
-    tmp[7] = tx_power_s[cfg.radio.tx_power] | (cfg.serial.enable_mode_switch ? 1 : 0) << 2 | (cfg.serial.enable_RSSI_cmds ? 1 : 0) << 5 | frame_size_s[cfg.transport.frame_size] << 6
-    tmp[8] = cfg.radio.channel
-    tmp[9] = (cfg.transport.enable_LBT ? 1 : 0) << 4 | (cfg.mode === 'relay' ? 1 : 0) << 5 | (cfg.transport.fixed ? 1 : 0) << 6 | (cfg.transport.enable_RSSI ? 1 : 0) << 7
-    return tmp
-}
 
-/**
- * De-serialize a byte array into a configuration json.
- * While the original specs would allow to read registers in chunks, here it is assumed to be in one operation.
- * @param array the returned array of bytes
- * @param skip_sig_check ignore checks on the return header (used for internal tests only)
- * @returns the de-serialized json
- */
-export function deserialize_config(array: Uint8Array, skip_sig_check = false): lora_cfg_t {
-    if (!skip_sig_check && array[0] !== 0xC1 && array[1] !== 0x00 && array[2] !== 0x08) throw new Error("Unable to decode an arbitrary received message.")
-    const cfg: lora_cfg_t = { radio: {}, serial: {}, transport: {} } as lora_cfg_t
+    /**
+     * send C0/C2 BASE LENGHT DATA
+     * recv C1 BASE LENGTH DATA
+     * @param base 
+     * @param data 
+     * @param temporary 
+     */
+    write_registers(base: number, data: Uint8Array, temporary = false) { }
 
-    cfg.mode = (((array[9] >> 5) & 1) === 0) ? 'endpoint' : 'relay'
-    if (cfg.mode === 'endpoint') {
-        cfg.address = array[3] << 8 + array[4]
-        cfg.network = array[5]
+    /**
+     * send C1 BASE LENGTH
+     * recv C1 BASE LENGTH DATA
+     * @param base 
+     */
+    read_registers(base: number): Uint8Array { }
+
+
+    /**
+     * Request device information (read-only registers)
+     */
+    get_pid() { const payload = new Uint8Array([0xC1, 0x80, 0x6]) }
+
+    /**
+     * Set a specific crypto key (write-only registers)
+     * @param key crypto key selected
+     * @returns the command to send.
+     */
+    set_crypto(key: number): Uint8Array {
+        const tmp = new Uint8Array(3 + 2)
+        tmp[0] = 0xC0
+        tmp[1] = 0x00
+        tmp[2] = 0x08
+        tmp[3] = key & 0xff00
+        tmp[4] = key & 0x00ff
+        return tmp;
     }
-    else {
-        cfg.src_network = array[3]
-        cfg.dst_network = array[4]
+
+    /**
+     * Switch operating mode of the modem (transmission/configuration).
+     * It only works if `enable_mode_switch` is set to true
+     */
+    switch_modem_mode(mode: 'transmission' | 'configuration') {
+        const tmp = new Uint8Array([0xC0, 0xC1, 0xC2, 0xC3, 0x02, mode === 'transmission' ? 0 : 1])
     }
-    cfg.radio.air_rate = air_rate_e[array[6] & 1]
-    cfg.serial.mode = mode_e[((array[6] >> 3) & 3)]
-    cfg.serial.baud_rate = baud_rate_e[((array[6] >> 5) & 7)]
-
-    cfg.radio.tx_power = tx_power_e[array[7] & 3]
-    cfg.serial.enable_mode_switch = !(((array[7] >> 2) & 1) === 0)
-    cfg.serial.enable_RSSI_cmds = !(((array[7] >> 5) & 1) === 0)
-    cfg.transport.frame_size = frame_size_e[(array[7] >> 6) & 3]
-
-    cfg.radio.channel = array[8]
-
-    cfg.transport.enable_LBT = !(((array[9] >> 4) & 1) === 0)
-    cfg.transport.fixed = !(((array[9] >> 6) & 1) === 0)
-    cfg.transport.enable_RSSI = !(((array[9] >> 7) & 1) === 0)
-    return cfg
 }
 
-/**
- * Generate the command to send to set a specific crypto key (write-only registers)
- * @param key crypto key selected
- * @returns the command to send.
- */
-export function set_crypto(key: number): Uint8Array {
-    const tmp = new Uint8Array(3 + 2)
-    tmp[0] = 0xC0
-    tmp[1] = 0x00
-    tmp[2] = 0x08
-    tmp[3] = key & 0xff00
-    tmp[4] = key & 0x00ff
-    return tmp;
-}
 
-/**
- * Generate the command to request the pid (read-only registers)
- */
-export function get_pid() {
-    return new Uint8Array([0xC0, 0x80, 0x6])
-}
 
-/**
- * Generate the command to switch operating mode of the modem (transmission/configuration).
- * It only works if `enable_mode_switch` is set to true
- */
-export function switch_modem_mode(mode: 'transmission' | 'configuration') {
-    return new Uint8Array([0xC0, 0xC1, 0xC2, 0xC3, 0x02, mode === 'transmission' ? 0 : 1])
-}
+
+
 
 //////////////
 
@@ -265,10 +293,10 @@ function test() {
     }) as lora_cfg_t;
 
     console.log(w, Value.Check(lora_cfg_relay, w));
-    console.log(serialize_config(w));
-    console.log(deserialize_config(serialize_config(w), false));
+    console.log(lora_node.serialize_config(w));
+    console.log(lora_node.deserialize_config(lora_node.serialize_config(w)));
     console.log(
-        serialize_config(deserialize_config(serialize_config(w), false)),
+        lora_node.serialize_config(lora_node.deserialize_config(lora_node.serialize_config(w))),
     );
 }
 
